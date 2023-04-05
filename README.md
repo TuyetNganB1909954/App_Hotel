beb24d9f11ec1630c2282e0c79c82d0ec32ce22b
NguyenThiTuyetNganB1909954 <tuyetngan241tn@gmail.com>
2023-04-05 22:07:10 +0700

    Doi theme cho ung dung

---
+++ README.md
@@ -0,0 +1,9 @@
+# myshop
+
+Bài tập thực hành CT484, HK2 2022-2023
+
+**MSSV**: B1909954
+
+**Họ tên SV**: Nguyễn Thị Tuyết Ngân
+
+**Nhóm học phần**: CT48403
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+
+Future<void> main() async {
+  await dotenv.load();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Lato',
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.purple,
+        ).copyWith(secondary: Colors.deepOrange),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('aaaa'),
+        ),
+        body: const Center(
+          child: Text('data'),
+        ),
+      ),
+    );
+  }
+}
